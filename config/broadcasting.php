653ef572174975ace2b3fe59db9fba8803689a61
Ulugbek Ahmedov <mdsolutions@mail.ru>
2022-07-02 14:48:50 +0500

    fix websockets

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,9 +40,12 @@ return [
                 'encrypted' => true,
                 'host' => '127.0.0.1',
                 'port' => 6001,
-                'scheme' => 'http',
-
-                // 'useTLS' => true,
+                'scheme' => 'https',
+                'useTLS' => true,
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ],
             ],
         ],
 
